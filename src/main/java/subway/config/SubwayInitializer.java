eafdb74ccd43ee028af1f22fad451e44db01a3e5
OhYeHyun <ohyhohyl@naver.com>
2024-11-19 18:33:39 +0900

    fix(initializer): 인덱스 값 수정

--- src/main/java/subway/config/SubwayInitializer.java
+++ src/main/java/subway/config/SubwayInitializer.java
@@ -13,7 +13,7 @@ import subway.domain.StationRepository;
 public class SubwayInitializer {
     public static final String[] STATIONS = {"교대역", "강남역", "역삼역", "남부터미널역", "양재시민의숲역", "매봉역"};
     public static final String[] LINES = {"2호선", "3호선", "신분당선"};
-    public static final String[] LINE_INFO1 = {"2호선", "교대역", "역삼역"};
+    public static final String[] LINE_INFO1 = {"2호선", "교대역", "강남역", "역삼역"};
     public static final String[] LINE_INFO2 = {"3호선", "교대역", "남부터미널역", "양재역", "매봉역"};
     public static final String[] LINE_INFO3 = {"신분당선", "강남역", "양재역", "양재시민의숲역"};
     public static final String[][] LINE_INFOS = {LINE_INFO1, LINE_INFO2, LINE_INFO3};
@@ -40,7 +40,7 @@ public class SubwayInitializer {
     private static void initializeMap() {
         for (String[] lineInfo : LINE_INFOS) {
             Line line = generateLine(lineInfo[0]);
-            String[] stations = Arrays.copyOfRange(lineInfo, 1, lineInfo.length - 1);
+            String[] stations = Arrays.copyOfRange(lineInfo, 1, lineInfo.length);
 
             MapRepository.addLineInfo(generateLineInfo(line, makeStations(stations)));
         }
