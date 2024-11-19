d4b08ef3d9cf491741c863e3f4142ebf11d75105
OhYeHyun <ohyhohyl@naver.com>
2024-11-19 19:07:26 +0900

    test(StationService): 역 관리 기능 테스트

---
+++ src/test/java/subway/service/StationServiceTest.java
@@ -0,0 +1,36 @@
+package subway.service;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import subway.domain.Station;
+import subway.domain.StationRepository;
+
+class StationServiceTest {
+
+    @Test
+    @DisplayName("역 추가가 올바르게 되는지 확인")
+    void 역_추가_테스트() {
+        StationService.addStation("교대역");
+        StationService.addStation("강남역");
+
+        List<Station> result = StationRepository.stations();
+
+        assertThat(result.stream().map(Station::getName)).containsExactly("교대역", "강남역");
+    }
+
+    @Test
+    @DisplayName("역 삭제가 올바르게 되는지 확인")
+    void 역_삭제_테스트() {
+        StationService.addStation("교대역");
+        StationService.addStation("강남역");
+        StationService.deleteStation("교대역");
+        StationService.deleteStation("강남역");
+
+        List<Station> result = StationRepository.stations();
+
+        assertThat(result.stream().map(Station::getName)).containsExactly();
+    }
+}
