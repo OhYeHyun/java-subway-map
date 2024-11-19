428fc512bfb1e28c13183d3e8fc16cd354595b33
OhYeHyun <ohyhohyl@naver.com>
2024-11-19 15:58:23 +0900

    feat(NumberValidator): 숫자 입력값 확인 기능

---
+++ src/main/java/subway/validator/NumberValidator.java
@@ -0,0 +1,17 @@
+package subway.validator;
+
+public class NumberValidator {
+
+    public static int validate(String input) {
+        validateIsNumeric(input);
+        return Integer.parseInt(input);
+    }
+
+    private static void validateIsNumeric(String input) {
+        try {
+            Integer.parseInt(input);
+        } catch (NumberFormatException e) {
+            throw new IllegalArgumentException(ValidatorErrorMessage.MUST_BE_NUMBER.getMessage());
+        }
+    }
+}
