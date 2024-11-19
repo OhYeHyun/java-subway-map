37ba8fa8c0fabf036ae4862e57173680dcc0596f
OhYeHyun <ohyhohyl@naver.com>
2024-11-19 15:53:57 +0900

    feat(ErrorMessage): errorMessage enum 클래스 추가

---
+++ src/main/java/subway/validator/ValidatorErrorMessage.java
@@ -0,0 +1,19 @@
+package subway.validator;
+
+public enum ValidatorErrorMessage {
+    ERROR_MESSAGE_BEGINNING("[ERROR] "),
+    MUST_BE_NUMBER("숫자로 입력해주세요."),
+    MUST_BE_NUMBER_IN_MENU("해당 번호의 메뉴가 존재하지 않습니다."),
+    MUST_BE_CHARACTER("문자로 입력해주세요."),
+    MUST_BE_EXISTING_STATION("역 이름이 존재하지 않습니다.");
+
+    private final String message;
+
+    ValidatorErrorMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return ERROR_MESSAGE_BEGINNING.message + message;
+    }
+}
