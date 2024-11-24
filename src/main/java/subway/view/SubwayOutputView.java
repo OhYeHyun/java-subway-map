1cd72167604fdbb85b0e88a0ba0885cdae236951
OhYeHyun <ohyhohyl@naver.com>
2024-11-24 22:15:26 +0900

    refactor(outputView): 빈 줄과 함께 출력문을 출력하는 함수 추가
    - printLineSpace() 함수 추가

--- src/main/java/subway/view/SubwayOutputView.java
+++ src/main/java/subway/view/SubwayOutputView.java
@@ -17,119 +17,101 @@ public class SubwayOutputView {
         System.out.print(System.lineSeparator());
     }
 
-    public static void selectOption() {
+    public static void printWithLineSpace(String text) {
         printLineSpace();
-        print(Instruction.SELECT_OPTION.getInstruction());
+        print(text);
+    }
+
+    public static void selectOption() {
+        printWithLineSpace(Instruction.SELECT_OPTION.getInstruction());
     }
 
     public static void displayStation() {
-        printLineSpace();
-        print(Instruction.DISPLAY.getInstruction(Target.STATION_LIST.getTarget()));
+        printWithLineSpace(Instruction.DISPLAY.getInstruction(Target.STATION_LIST.getTarget()));
     }
 
     public static void displayLine() {
-        printLineSpace();
-        print(Instruction.DISPLAY.getInstruction(Target.LINE_LIST.getTarget()));
+        printWithLineSpace(Instruction.DISPLAY.getInstruction(Target.LINE_LIST.getTarget()));
     }
 
     public static void displayMap() {
-        printLineSpace();
-        print(Instruction.DISPLAY.getInstruction(Target.MAP_LIST.getTarget()));
+        printWithLineSpace(Instruction.DISPLAY.getInstruction(Target.MAP_LIST.getTarget()));
     }
 
     public static void inputStationToAdd() {
-        printLineSpace();
-        print(Instruction.INPUT_TO_ADD.getInstruction(Target.STATION_NAME.getTarget()));
+        printWithLineSpace(Instruction.INPUT_TO_ADD.getInstruction(Target.STATION_NAME.getTarget()));
     }
 
     public static void inputStationToDelete() {
-        printLineSpace();
-        print(Instruction.INPUT_TO_DELETE.getInstruction(Target.STATION_NAME.getTarget()));
+        printWithLineSpace(Instruction.INPUT_TO_DELETE.getInstruction(Target.STATION_NAME.getTarget()));
     }
 
     public static void inputLineToAdd() {
-        printLineSpace();
-        print(Instruction.INPUT_TO_ADD.getInstruction(Target.LINE_NAME.getTarget()));
+        printWithLineSpace(Instruction.INPUT_TO_ADD.getInstruction(Target.LINE_NAME.getTarget()));
     }
 
     public static void inputUpwardToAdd() {
-        printLineSpace();
-        print(Instruction.INPUT_TO_ADD.getInstruction(Target.UPWARD_STATION_NAME.getTarget()));
+        printWithLineSpace(Instruction.INPUT_TO_ADD.getInstruction(Target.UPWARD_STATION_NAME.getTarget()));
     }
 
     public static void inputDownwardToAdd() {
-        printLineSpace();
-        print(Instruction.INPUT_TO_ADD.getInstruction(Target.DOWNWARD_STATION_NAME.getTarget()));
+        printWithLineSpace(Instruction.INPUT_TO_ADD.getInstruction(Target.DOWNWARD_STATION_NAME.getTarget()));
     }
 
     public static void inputLineToDelete() {
-        printLineSpace();
-        print(Instruction.INPUT_TO_DELETE.getInstruction(Target.LINE_NAME.getTarget()));
+        printWithLineSpace(Instruction.INPUT_TO_DELETE.getInstruction(Target.LINE_NAME.getTarget()));
     }
 
     public static void inputLineToAddSection() {
-        printLineSpace();
-        print(Instruction.INPUT_SECTION.getInstruction(Target.LINE.getTarget()));
+        printWithLineSpace(Instruction.INPUT_SECTION.getInstruction(Target.LINE.getTarget()));
     }
 
     public static void inputStationToAddSection() {
-        printLineSpace();
-        print(Instruction.INPUT_SECTION.getInstruction(Target.STATION.getTarget()));
+        printWithLineSpace(Instruction.INPUT_SECTION.getInstruction(Target.STATION.getTarget()));
     }
 
     public static void inputOrderToAddSection() {
-        printLineSpace();
-        print(Instruction.INPUT_SECTION.getInstruction(Target.ORDER.getTarget()));
+        printWithLineSpace(Instruction.INPUT_SECTION.getInstruction(Target.ORDER.getTarget()));
     }
 
     public static void inputLineToDeleteSection() {
-        printLineSpace();
-        print(Instruction.INPUT_TO_DELETE.getInstruction(Target.LINE_OF_SECTION.getTarget()));
+        printWithLineSpace(Instruction.INPUT_TO_DELETE.getInstruction(Target.LINE_OF_SECTION.getTarget()));
     }
 
     public static void inputStationToDeleteSection() {
-        printLineSpace();
-        print(Instruction.INPUT_TO_DELETE.getInstruction(Target.STATION_OF_SECTION.getTarget()));
+        printWithLineSpace(Instruction.INPUT_TO_DELETE.getInstruction(Target.STATION_OF_SECTION.getTarget()));
     }
 
     public static void noticeNotFoundOption() {
-        printLineSpace();
-        print(ValidatorErrorMessage.MUST_BE_NUMBER_IN_MENU.getMessage());
+        printWithLineSpace(ValidatorErrorMessage.MUST_BE_NUMBER_IN_MENU.getMessage());
     }
 
     public static void noticeStationAdded() {
-        printLineSpace();
-        print(Service.ADD.getInfo(Target.STATION_OF_SUBWAY.getTarget()));
+        printWithLineSpace(Service.ADD.getInfo(Target.STATION_OF_SUBWAY.getTarget()));
     }
 
     public static void noticeStationDeleted() {
-        printLineSpace();
-        print(Service.DELETE.getInfo(Target.STATION_OF_SUBWAY.getTarget()));
+        printWithLineSpace(Service.DELETE.getInfo(Target.STATION_OF_SUBWAY.getTarget()));
     }
 
     public static void noticeLineAdded() {
-        printLineSpace();
-        print(Service.ADD.getInfo(Target.LINE_OF_SUBWAY.getTarget()));
+        printWithLineSpace(Service.ADD.getInfo(Target.LINE_OF_SUBWAY.getTarget()));
     }
 
     public static void noticeLineDeleted() {
-        printLineSpace();
-        print(Service.DELETE.getInfo(Target.LINE_OF_SUBWAY.getTarget()));
+        printWithLineSpace(Service.DELETE.getInfo(Target.LINE_OF_SUBWAY.getTarget()));
     }
 
     public static void noticeSectionAdded() {
-        printLineSpace();
-        print(Service.ADD.getInfo(Target.SECTION_OF_SUBWAY.getTarget()));
+        printWithLineSpace(Service.ADD.getInfo(Target.SECTION_OF_SUBWAY.getTarget()));
     }
 
     public static void noticeSectionDeleted() {
-        printLineSpace();
-        print(Service.DELETE.getInfo(Target.SECTION_OF_SUBWAY.getTarget()));
+        printWithLineSpace(Service.DELETE.getInfo(Target.SECTION_OF_SUBWAY.getTarget()));
     }
 
     public static void printMenu(Menu menu) {
-        printLineSpace();
-        print(Instruction.TITLE.getInstruction(menu.getTitle()));
+        printWithLineSpace(Instruction.TITLE.getInstruction(menu.getTitle()));
 
         for (int i = 0; i < menu.getOptions().length; i++) {
             print(OptionList.OPTION.getOption(String.valueOf(i + 1), menu.getOptions()[i]));
